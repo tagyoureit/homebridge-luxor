011457fa97417866a1d882c2a8c70e0c1f5604a5
Russ Goldin <russ.goldin@gmail.com>
2016-04-29 11:36:29 -0700

    Convert from Accessory to Platform and many other changes

--- README.md
+++ README.md
@@ -1,30 +1,28 @@
 # homebridge-luxor
 
-This is a *BETA* module for the [HomeBridge Platform](https://github.com/nfarina/homebridge) to control [FX Luminaire](http://www.FXL.com).
+This is a PLATFORM module for the [HomeBridge Platform](https://github.com/nfarina/homebridge) to control [FX Luminaire](http://www.FXL.com).  Huge props to [David Parry](https://github.com/devbobo) for converting this from an accessory to a platform in the blink of an eye!
 
-This plug-in enables power and brightness controls for [FX Luminaire Luxor ZD](http://www.fxl.com/product/power-and-control/luxor-zd) light groups.  There are a few know issues which are listed below and would appreciate help in fixing them!  :-)
-
+This plug-in enables power and brightness controls for [FX Luminaire Luxor ZD](http://www.fxl.com/product/power-and-control/luxor-zd) light groups.  
 
 # Installation
 
 1. Install homebridge using: npm install -g homebridge [Full directions at HomeBridge page](https://github.com/nfarina/homebridge)
-2. Install this plugin using: npm install -g homebridge-wemo
+2. Install this plugin using: npm install -g homebridge-luxor
 3. Update your configuration file. See sample-config.json in this repository for a sample. 
-    - The name and groupName should be unique for each lighting group that you have or HomeBridge will think it is the same device and try to assign it the same UUID and throw an error.  (See enhancement #2 below.)
 
 
 # Known Issues
 1.  Plug-in is written only for the ZD controller (Zoning, Diming).  It may work with the ZDC (Zoning, Dimming, and Color) but I have not tested it (the color selection is not currently coded).  (See #1 in enhancements.)
-2.  FIXED ~When using the app, Program A changes the on/off or brightness, Program B will need to refresh twice to see the changes take effect.  This is because the getBrightness and getPowerOn functions return their values before the http post returns with its data.~  
+2.  FIXED ~~When using the app, Program A changes the on/off or brightness, Program B will need to refresh twice to see the changes take effect.  This is because the getBrightness and getPowerOn functions return their values before the http post returns with its data.~~ 
 3.  Refactor code.
 
 
 # Future enhancements (in no particular order)
 1.  Add ZDC configurations/code (if there is a demand)
-2.  Figure out the differences between the name, servicename, accessoryname, etc to enable proper HomeKit configuration and unique UUID
-3.  Add manufacturer and model (at the service level?) for various apps' aesthetics.
-4.  Module should be able to dynamically retrieve groupname and groupnumber (as this is exposed via an API) but for now it is in the configuration file.
-5.  This module should really be converted to a platform.  Effect will be that one controller can be defined in the configuration file and all the lights will be automatically added.
+2.  FIXED ~~Figure out the differences between the name, servicename, accessoryname, etc to enable proper HomeKit configuration and unique UUID~~
+3.  FIXED ~~Add manufacturer and model (at the service level?) for various apps' aesthetics.~~ (Well, manufacturer is added anyway.)
+4.  FIXED ~~Module should be able to dynamically retrieve groupname and groupnumber (as this is exposed via an API) but for now it is in the configuration file.~~
+5.  FIXED ~~This module should really be converted to a platform.  Effect will be that one controller can be defined in the configuration file and all the lights will be automatically added.~~
 6?. Luxor controllers have Themes (preset configurations for groups of lights) but not sure if these should be included as they could duplicate the Zones functionality in HomeKit.  Possibly Zones could be configured programatically but I haven't looked into that.
 7.  There is no push notification (eg if I turn on the light manually at the controller, or via the native app, HomeKit won't get notified).  Should I implement a timer to periodically check the state of the lights and update HomeKit?
 
@@ -33,5 +31,6 @@ This plug-in enables power and brightness controls for [FX Luminaire Luxor ZD](h
 
 1.  I knew that the FX controller had a minimal web interface, and discovered a couple of the API calls, but then found a full(?) list and implementation of the code in a Go library written by [Scott Lamb](https://github.com/scottlamb/luxor).
 2.  I used the original WeMo code from [rudders](https://github.com/rudders/homebridge-wemo) as a template and hacked away at it until I got to this point.
-3.  Of course, to [nfarina](https://github.com/nfarina/homebridge) for the HomeBridge and, in turn, [KhaosT](http://twitter.com/khaost) for the original [HAP-NodeJS](https://github.com/KhaosT/HAP-NodeJS) project.  
+3.  Of course, to [nfarina](https://github.com/nfarina/homebridge) for the HomeBridge and, in turn, [KhaosT](http://twitter.com/khaost) for the original [HAP-NodeJS](https://github.com/KhaosT/HAP-NodeJS) project. 
+4.  [David Parry](https://github.com/devbobo) for helping convert this to a platform module.
 
--- index.js
+++ index.js
@@ -1,37 +1,88 @@
-/*jslint node: true */
+/* jshint node: true */
+
 "use strict";
 
-var Service, Characteristic;
+var Accessory, Characteristic, Service, UUIDGen;
+
 var request = require('request');
 var rp = require('request-promise');
 var logmore = false; //false for less; true for more
 
-
 module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
+    Accessory = homebridge.platformAccessory;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-luxor", "Luxor", LuxorAccessory);
+    Service = homebridge.hap.Service;
+    UUIDGen = homebridge.hap.uuid;
+
+    homebridge.registerPlatform("homebridge-luxor", "Luxor", LuxorPlatform, true);
 };
 
+function LuxorPlatform(log, config, api) {
+    this.config = config || {};
 
-function LuxorAccessory(log, config) {
+    this.api = api;
+    this.accessories = {};
     this.log = log;
-    this.name = config["name"] || this.name;
-    this.service = "Lights"; // how is this used??? 
-    //this.groupName = config["groupName"] || this.name; // fallback to "name" if you didn't specify an exact "groupName" --> should be able to set this automatically, but how to bind it afterwards? 
-    var groupName;
-    this.groupNumber = config["groupNumber"] || 1; //Luxor group numbers start at 1
-    this.ip_addr = config["ipAddr"]; //mandatory
-    var controller; //optional, will probably remove as we can get this programatically.  Maybe set to name?
-    var binaryState; // will set programmatically via groupListGet below
-    var brightness; //brightness (0-100), will set programmatically via groupListGet below
-    this.device = null; //will be instance of lightbulb that we control.
-    this.log("Starting a Luxor light with name %s in %s'", this.groupName, this.name);
-
+    
+    this.ip_addr = config.ipAddr; 
+    
     this.search();
 }
 
-LuxorAccessory.prototype.search = function () {
+LuxorPlatform.prototype.addAccessory = function(uuid, name, group, brightness) {
+    this.log("Found: %s [Group %d]", name, group);
+
+    var accessory = new Accessory(name, uuid);
+    
+    accessory
+        .getService(Service.AccessoryInformation)
+        .setCharacteristic(Characteristic.Manufacturer, "Luxor");
+        
+    accessory
+        .addService(Service.Lightbulb)
+        .addCharacteristic(Characteristic.Brightness);
+
+    this.accessories[accessory.UUID] = new LuxorAccessory(this.log, accessory, this.ip_addr, group, brightness);
+    this.api.registerPlatformAccessories("homebridge-luxor", "Luxor", [accessory]);
+};
+
+LuxorPlatform.prototype.configureAccessory = function(accessory) {
+    this.accessories[accessory.UUID] = accessory;
+};
+
+LuxorPlatform.prototype.groupListGet = function (callback, whichcall) {
+    var self = this;
+    if (logmore) {
+        self.log('Retrieving light groups');
+    }
+
+    var post_options = {
+        url: 'http://' + self.ip_addr + '/GroupListGet.json',
+        method: 'POST'
+    };
+
+    rp.post(post_options, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            for (var i in info.GroupList) {
+                var item = info.GroupList[i];
+                var uuid = UUIDGen.generate(item.Name);
+                var accessory = self.accessories[uuid];
+            
+                if (accessory === undefined) {
+                    self.addAccessory(uuid, item.Name, item.GroupNumber, item.Intensity);
+                }
+                else if (accessory instanceof Accessory) {
+                    self.accessories[accessory.UUID] = new LuxorAccessory(self.log, accessory, self.ip_addr, item.GroupNumber, item.Intensity);
+                }
+            }
+        } else {
+            throw new Error("Was not able to connect to the controller.  Check your IP Address. Error: " + err);
+        }
+    });
+};
+
+LuxorPlatform.prototype.search = function () {
     if (!this.ip_addr) {
         throw new Error(this.Name + " needs an IP Address in the config file.  Please see sample_config.json.");
     }
@@ -43,66 +94,84 @@ LuxorAccessory.prototype.search = function () {
         "method": "POST"
     };
 
-    var that = this;
+    var self = this;
     rp.post(post_options, function (err, response, body) {
         if (!err && response.statusCode == 200) {
             var info = JSON.parse(body);
-            that.controller = info["Controller"];
-            that.log('Found Controller name: ' + info["Controller"]);
+            self.controller = info.Controller;
+            self.log('Found Controller name: ' + info.Controller);
         } else {
-            throw new Error(that.Name + " was not able to connect to connect to the controller.  Check your IP Address.");
+            throw new Error(self.accessory.displayName + " was not able to connect to connect to the controller.  Check your IP Address.");
         }
 
         }).then(function (body) {
 
-            that.groupListGet(null, null, "initial");
+            self.groupListGet();
         });
+};
+
+function LuxorAccessory(log, accessory, ip_addr, group, brightness) {
+    var self = this;
+
+    this.accessory = accessory;
+    this.log = log;
+    
+    this.ip_addr = ip_addr;
+    this.groupNumber = group;
+    this.brightness = brightness;
+    this.binaryState = brightness > 0 ? 1 : 0;
+
+    this.accessory.on('identify', function(paired, callback) {
+        self.log("%s - identify", self.accessory.displayName);
+        callback();
+    });
+    
+    var service = this.accessory.getService(Service.Lightbulb);
+    
+    service.getCharacteristic(Characteristic.On)
+        .on('get', this.getPowerOn.bind(this))
+        .on('set', this.setPowerOn.bind(this));
+
+    service.getCharacteristic(Characteristic.Brightness)
+        .on('set', this.setBrightness.bind(this))
+        .on('get', this.getBrightness.bind(this));
 }
 
 LuxorAccessory.prototype.getPowerOn = function (callback) {
     if (logmore) {
-        this.log("In getPowerOn")
-    };
-    this.groupListGet(callback, this.brightness, "power");
-   
-}
+        this.log("In getPowerOn");
+    }
+    this.getCurrentState(callback, "power");
+};
 
 LuxorAccessory.prototype.setPowerOn = function (powerOn, callback) {
     this.binaryState = powerOn ? 1 : 0;
-    this.log("Setting %s to %s (default 50%)", this.groupName, this.binaryState == 1 ? "on" : "off" );
-    this.illuminateGroup(this.binaryState * 50); //set to 0 if we want to turn off, or 50 if we want to turn on.
-    callback(null);
-}
+    if (logmore){
+        this.log("Attepmting to set Power for the %s to %s", this.accessory.displayName, this.binaryState == 1 ? "on (default 50%)" : "off" );        
+    }
+    this.illuminateGroup(callback,this.binaryState * 50);  //set to 0 if we want to turn off, or 50 if we want to turn on.
+};
 
 LuxorAccessory.prototype.getBrightness = function (callback) {
     if (logmore) {
-        this.log("In getBrightness")
-    };
-    this.groupListGet(callback, this.brightness, "brightness");
-}
+        this.log("In getBrightness");
+    }
+    this.getCurrentState(callback, "brightness");
+};
 
 LuxorAccessory.prototype.setBrightness = function (brightness, callback) {
     if (logmore) {
-        this.log("In setBrightness")
-    };
-    /*if (this.brightness = 0) {
-        //update characteristic(on)
-        accessory.getService(Service.Lightbulb).getCharacteristic(Characteristic.On)
-        this.getService(Service.Lightbulb).getCharacteristic(Characteristic.On)
-    };*/ //To do...
-    this.brightness = this.illuminateGroup(brightness);
-    this.binaryState = this.brightness > 0 ? 1 : 0;
-    this.log("Set Brightness for the '%s' to %s", this.groupName, this.brightness);
-    callback(null);
-    
-}
+        this.log("Attempting to Set Brightness for the '%s' to %s", this.accessory.displayName, brightness);
+    }
+    this.illuminateGroup(callback, brightness);
+};
 
 
 LuxorAccessory.prototype.getServices = function () {
-    var lightbulbService = new Service.Lightbulb(this.groupName);
+    var lightbulbService = new Service.Lightbulb(this.accessory.displayName);
     if (logmore) {
-        this.log("Setting services for: " + this.groupName)
-    };
+        this.log("Setting services for: " + this.accessory.displayName);
+    }
 
     lightbulbService.getCharacteristic(Characteristic.On)
         .on('get', this.getPowerOn.bind(this))
@@ -110,116 +179,124 @@ LuxorAccessory.prototype.getServices = function () {
 
     lightbulbService.getCharacteristic(Characteristic.Brightness)
         .on('set', this.setBrightness.bind(this))
-        .on('get', this.getBrightness.bind(this))
+        .on('get', this.getBrightness.bind(this));
 
     return [lightbulbService];
-}
+};
 
 
 function getStatus(result) {
-
     switch (result) {
-    case 0:
-        return ('Ok'); //StatusOk
-    case (1):
-        return ('Unknown Method'); //StatusUnknownMethod
-    case (101):
-        return ('Unparseable Request'); //StatusUnparseableRequest
-    case (102):
-        return ('Invalid Request'); //StatusInvalidRequest
-    case (201):
-        return ('Precondition Failed'); //StatusPreconditionFailed
-    case (202):
-        return ('Group Name In Use'); //StatusGroupNameInUse
-    case (205):
-        return ('Group Number In Use'); //StatusGroupNumberInUse
-    case (243):
-        return ('Theme Index Out Of Range'); //StatusThemeIndexOutOfRange
-    default:
-        return ('Unknown status');
+        case 0:
+            return ('Ok'); //StatusOk
+        case (1):
+            return ('Unknown Method'); //StatusUnknownMethod
+        case (101):
+            return ('Unparseable Request'); //StatusUnparseableRequest
+        case (102):
+            return ('Invalid Request'); //StatusInvalidRequest
+        case (201):
+            return ('Precondition Failed'); //StatusPreconditionFailed
+        case (202):
+            return ('Group Name In Use'); //StatusGroupNameInUse
+        case (205):
+            return ('Group Number In Use'); //StatusGroupNumberInUse
+        case (243):
+            return ('Theme Index Out Of Range'); //StatusThemeIndexOutOfRange
+        default:
+            return ('Unknown status');
     }
 }
+LuxorAccessory.prototype.illuminateGroup = function (callback, desiredIntensity) {
+        var self = this;
+        if (logmore) {
+            self.log('Setting light %s (%s) to intensity ', self.accessory.displayName, self.groupNumber, (desiredIntensity == 0 ? "0 (Off)" : desiredIntensity));
+        }
 
-LuxorAccessory.prototype.illuminateGroup = function (desiredIntensity) {
-    var that = this;
-    if (logmore) {
-        that.log('Setting light ' + that.groupName + ' to intensity ' + desiredIntensity)
-    };
-
-    var requestData = JSON.stringify({
-        'GroupNumber': that.groupNumber,
-        'Intensity': desiredIntensity
-    });
-    var result;
-    request({
-            url: 'http://' + that.ip_addr + '/IlluminateGroup.json',
-            method: "POST",
-            body: requestData,
-            headers: {
-                'cache-control': 'no-cache',
-                'content-type': 'text/plain',
-                'Content-Length': Buffer.byteLength(requestData)
-            },
-        },
-        function (error, response, body) {
-            if (error) {
-                that.log('Error setting intesity for ' + that.groupName + ': ' + error);
-            }
-            result = getStatus(JSON.parse(body).Status);
-            if (result == "Ok") {
-                that.log('Request to set %s intensity to %s: %s ', that.groupName, desiredIntensity, result);
-                that.brightness = desiredIntensity;
-                that.binaryState = (that.brightness) > 0 ? 1 : 0;
-            } else {
-                that.log('Something went wrong!  Request to set %s intensity to %s: %s ', that.groupName, desiredIntensity, result);
-            }
+        var requestData = JSON.stringify({
+            'GroupNumber': self.groupNumber,
+            'Intensity': desiredIntensity
         });
+        var result;
+        rp({
+                url: 'http://' + self.ip_addr + '/IlluminateGroup.json',
+                method: "POST",
+                body: requestData,
+                headers: {
+                    'cache-control': 'no-cache',
+                    'content-type': 'text/plain',
+                    'Content-Length': Buffer.byteLength(requestData)
+                }
+            },
+            function (error, response, body) {
+                if (error) {
+                    self.log('Error setting intesity for ' + self.accessory.displayName + ': ' + error);
+                }
+                result = getStatus(JSON.parse(body).Status);
+                if (result == "Ok") {
+                    self.log('Request to set %s intensity to %s: %s ', self.accessory.displayName, (desiredIntensity == 0 ? "0 (Off)" : desiredIntensity), result);
+                self.brightness = desiredIntensity;
+                self.binaryState = (self.brightness) > 0 ? 1 : 0;
+            } else {
+                self.log('Something went wrong!  Request to set %s intensity to %s: %s ', self.accessory.displayName, desiredIntensity, result);
+            }
+        }).then(function (body) {
+        callback(null);
+    })
+    .catch(function (err) {
+        throw new Error(self.accessory.displayName + " Crash! Error: " + err);
+    });
 
-    return (result);
+
 };
 
-LuxorAccessory.prototype.groupListGet = function (callback, brightness, whichcall) {
-    var that = this;
+LuxorAccessory.prototype.getCurrentState = function (callback, whichcall) {
+    var self = this;
     if (logmore) {
-        that.log('Retrieving light group' + that.groupName + 'and their current status')
-    };
+        self.log('Retrieving current %s of light group %s (%s)', whichcall, self.accessory.displayName, self.groupNumber);
+    }
 
     var post_options = {
-        url: 'http://' + that.ip_addr + '/GroupListGet.json',
+        url: 'http://' + self.ip_addr + '/GroupListGet.json',
         method: 'POST'
 
     };
 
-    rp.post(post_options, function (err, response, body) {
+    rp(post_options, function (err, response, body) {
         if (!err && response.statusCode == 200) {
             var info = JSON.parse(body);
-            //var arrayindex = that.groupNumber-1;  // arrays start at 0 while luxor numbering starts at 1
-            if (that.groupNumber == info["GroupList"][that.groupNumber - 1].GroupNumber) {
-                that.groupName = info["GroupList"][that.groupNumber - 1].Name;
-                that.brightness = info["GroupList"][that.groupNumber - 1].Intensity;
-                that.binaryState = that.brightness > 0 ? 1 : 0;
+            //var arrayindex = self.groupNumber-1;  // arrays start at 0 while luxor numbering starts at 1
+            /*if (self.groupNumber == info.GroupList[self.groupNumber - 1].GroupNumber) {
+                self.accessory.displayName = info.GroupList[self.groupNumber - 1].Name;
+                self.brightness = info.GroupList[self.groupNumber - 1].Intensity;
+                self.binaryState = self.brightness > 0 ? 1 : 0;
 
             } else {
-                that.log("Could not match group number in config.json to controller groups");
+                self.log("Could not match group number in config.json to controller groups");
 
-            }
+            }*/
+            self.brightness = info.GroupList[self.groupNumber - 1].Intensity;  // JS arrays start at 0 while luxor numbering starts at 1
+            self.binaryState = self.brightness > 0 ? 1 : 0;
         } else {
-            throw new Error(that.name + " was not able to connect to connect to the controller.  Check your IP Address. Error: " + err);
+            throw new Error(self.accessory.displayName + " was not able to connect to the controller.  Check your IP Address. Error: " + err);
         }
     })
 
-
-    .then(function (body) {;
-        if (logmore) {
-            that.log('End of groupListGet, result = %s', that.brightness);
-            that.log('Whichcall = %s', whichcall)
-        };
+    .then(function (body) {
+        if (logmore) 
+        {
+             self.log('Successfully retrieved current %s of light group %s with result = %s', whichcall, self.accessory.displayName, (whichcall == "brightness" ? self.brightness: (self.binaryState==1?"On":"Off")));
+        }
+        
+           
         if (whichcall == "brightness") {
-            callback(null, that.brightness);
+            callback(null, self.brightness);
         } else if (whichcall == "power") {
-            callback(null, that.binaryState);
-        } else if (whichcall == "initial") {
-            that.log('Setting initial params');
-        };
+            callback(null, self.binaryState);
+        }
+        else {throw new Error(self.accessory.displayName + " Crash! Error: " + err);};
+    })
+    .catch(function (err) {
+    throw new Error(self.accessory.displayName + " Crash! Error: " + err);
     });
 };
